617e94c91771aeb92dd49db807a1fe9e02bbd196
yuzunzz <yuzunzz@163.com>
2016-05-20 10:33:54 +0000

    修改邮箱变量设置

--- config.py
+++ config.py
@@ -5,14 +5,16 @@ SECURITY_PASSWORD_SALT = 'my_s3cUr3_s4lt'
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite')
 
 # mail settings
-MAIL_SERVER = 'smtp.xxxx.com'
+MAIL_SERVER = 'smtp.aliyun.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
 
-MAIL_USERNAME = 'xxxxxxxxxxxx@xxxx.com'
-MAIL_PASSWORD = 'xxxxxxxxxxxxxxxxxxx'
+#export APP_MAIL_USERNAME=""
+#export APP_MAIL_PASSWORD=""
+MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
+MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
 
 # mail accounts
-MAIL_DEFAULT_SENDER = 'xxxx@xxxx.com'
+MAIL_DEFAULT_SENDER = 'yuzunzz@aliyun.com'
 
